5814608e09976c18307f5d6e16c2e7815ca57f77
Armand Deshais <armand.deshais@siqual.fr>
2022-05-09 16:12:20 +0200

    Refactor affichage listings

--- components/listing/Biglist/Biglist.js
+++ components/listing/Biglist/Biglist.js
@@ -1,12 +1,39 @@
-import {useState} from "react";
+import {DataGrid} from '@mui/x-data-grid';
 
 
-export default function Biglist() {
+export default function Biglist(props) {
 
-    const [paginate, setPaginate] = useState(1);
+    const columns = [];
+    const rows = [];
+    let keys = [];
+    if (!props.data.loading) {
+        for (const [key] of Object.entries(props.data.data[1])) {
+            keys.push(key);
+        }
 
-    const [data, setData] = useState({});
+        keys.map(k => {
+            columns.push({
+                field: k,
+                headerName: k,
+                sortable: true,
+                width: k === 'id' ? 90 : 200
 
-
-
+            })
+        })
+        props.data.data.map(r => {
+            rows.push(r)
+        })
+    }
+    const finalRows = Array.from(new Set(rows.map(JSON.stringify))).map(JSON.parse);
+    return (
+        <div style={{height: 600, width: '80%', margin: '0 auto'}}>
+            <DataGrid
+                columns={columns}
+                rows={finalRows}
+                rowsPerPageOptions={[5]}
+                pageSize={10}
+                checkboxSelection
+            />
+        </div>
+    )
 }
--- pages/agencies.js
+++ pages/agencies.js
@@ -1,40 +1,11 @@
-import React from "react";
-import * as constant from '../utils/constants';
+import GetAll from "../utils/api";
+import Biglist from "../components/listing/Biglist/Biglist";
 
-export default function Agencies({agencies}) {
-    return <>
-        <main>
-            <table className={"table container"}>
-                <thead className={"thead-dark"}>
-                <tr>
-                    <th>#</th>
-                    <th>code</th>
-                    <th>libelle</th>
-                </tr>
-                </thead>
-                <tbody>
-
-                {
-                    agencies.map((r) => (
-                        // eslint-disable-next-line react/jsx-key
-                        <tr>
-                            <th>{r.id}</th>
-                            <th>{r.code}</th>
-                            <th>{r.libelle}</th>
-                        </tr>
-                    ))
-                }
-                </tbody>
-            </table>
-        </main>
-    </>
-}
-
-export async function getStaticProps({params}) {
-    const agencies = await fetch(constant.URL_JAD_API + 'agencies').then(r => r.json())
-    return {
-        props: {
-            agencies
-        }
-    }
+export default function Agencies() {
+    const data = GetAll('agencies');
+    return (
+        <>
+            <Biglist data={data}/>
+        </>
+    )
 }
