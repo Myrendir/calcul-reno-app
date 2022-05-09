bfb161834519365cc9cbd9a31aa363644a396fbe
Armand Deshais <armand.deshais@siqual.fr>
2022-05-09 16:10:57 +0200

    ajout material-ui & refactor index

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
 import Head from "next/head";
 import 'bootstrap/dist/css/bootstrap.css'
+import Navbar from "../components/layout/Navbar/Navbar";
+
 function MyApp({Component, pageProps}) {
-    return (<>
+    const name = Component.title === undefined ? Component.name : Component.title;
+
+    return (
+        <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <title>Calcul Reno</title>
-            </Head><Component {...pageProps} /></>
+                <title>Calcul Reno - {name}</title>
+            </Head>
+            <Navbar/>
+            <Component {...pageProps} />
+        </>
+
     );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,7 @@
-import Link from 'next/link'
-import CreateArticle from "../components/form/CreateArticle"
+
 export default function Home() {
     return (
         <>
-            <nav className={"navbar container"}>
-                <Link href={'/agencies'}>
-                    <a className={"btn btn-primary"}>Agences</a>
-                </Link>
-                <Link href={'/articles'}>
-                    <a className={"btn btn-primary"}>Articles</a>
-                </Link>
-                <Link href={'/categories'}>
-                    <a className={"btn btn-primary"}>Catégories</a>
-                </Link>
-                <Link href={'/operations'}>
-                    <a className={"btn btn-primary"}>Opérations</a>
-                </Link>
-                <Link href={'/recipes'}>
-                    <a className={"btn btn-primary"}>Recettes</a>
-                </Link>
-                <Link href={'/workunits'}>
-                    <a className={"btn btn-primary"}>Chaînes de production</a>
-                </Link>
-            </nav>
-            <CreateArticle />
-
         </>
     )
 }
